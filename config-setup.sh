b103968beebf0c22ec5032004e335bc36d9c2f55
Jeff Stephens <jeffrey.stephens@readytalk.com>
2016-05-19 13:50:18 -0600

    Add setup script for Sublime Text 3

---
+++ config-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ln -s "$(pwd)"/sublime-text/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+ln -s "$(pwd)"/sublime-text/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
