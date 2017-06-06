07395649d43c37227275c7e2d8d11e088990828c
Jeff Stephens <jeffrey.stephens@readytalk.com>
2017-06-06 13:28:35 -0600

    Improve Sublime config setup to deal with existing files

--- config-setup.sh
+++ config-setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-ln -s "$(pwd)"/sublime-text/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-ln -s "$(pwd)"/sublime-text/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+packageControlPath=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+preferencesPath=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+
+echo $packageControlPath
+
+# back existing files up if they exist
+for oldFile in "$packageControlPath" "$preferencesPath"; do
+	if [ -f "$oldFile" ]; then
+		mv "$oldFile" "$oldFile.bak"
+		echo "Backed up existing configuration file at $oldFile.bak"
+	fi
+done
+
+echo "Symlinking Sublime Text preferences and package list..."
+ln -s "$(pwd)"/sublime-text/Package\ Control.sublime-settings "$packageControlPath"
+ln -s "$(pwd)"/sublime-text/Preferences.sublime-settings "$preferencesPath"
