da726921cb00da08238716a2517eea4215c90457
Jeff Stephens <jeff@onehopewine.com>
2020-11-12 16:58:19 -0800

    Update to new brew install script

--- brewinstall.sh
+++ brewinstall.sh
@@ -1,4 +1,4 @@
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew doctor; \
 brew install git; \
 brew install thefuck; \
