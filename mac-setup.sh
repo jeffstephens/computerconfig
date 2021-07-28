5bc0518d326d0d5afa09f9c4961da2a299c9983a
Jeff Stephens <jeff@onehopewine.com>
2021-07-28 13:03:16 -0700

    Remove Atom from mac setup

--- mac-setup.sh
+++ mac-setup.sh
@@ -21,27 +21,5 @@ if [ ! -f ~/.bash_profile_local ]; then
 	echo "Created ~/.bash_profile_local for machine-specific commands"
 fi
 
-# this might also work on Linux, but that hasn't been tested so it's here
-if [ -d ~/.atom ]; then
-	mkdir -p ~/.atom-backup
-	cp -r ~/.atom/* ~/.atom-backup
-
-	echo "Previous Atom files have been moved to ~/.atom-backup"
-
-	rm -rf ~/.atom/packages
-	rm ~/.atom/config.cson
-	rm ~/.atom/github.cson
-	rm ~/.atom/keymap.cson
-	rm ~/.atom/snippets.cson
-
-	ln -s "$(pwd)"/atom/packages ~/.atom/packages
-	ln -s "$(pwd)"/atom/config.cson ~/.atom/config.cson
-	ln -s "$(pwd)"/atom/github.cson ~/.atom/github.cson
-	ln -s "$(pwd)"/atom/keymap.cson ~/.atom/keymap.cson
-	ln -s "$(pwd)"/atom/snippets.cson ~/.atom/snippets.cson
-
-	echo "Atom configuration has been symlinked to this directory"
-fi
-
 echo "Installing fonts..."
 cp fonts/* /Library/Fonts/
